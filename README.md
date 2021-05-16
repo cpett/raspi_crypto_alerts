043d9f0fbc3b83ef048b6f05402067b8c088bc61
Cody <cpett26@gmail.com>
2021-05-15 20:59:20 -0500

    Update script to run on crontab schedule

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 This repo will help you set a script on your raspberry pi (or set it to run on your terminal in the background) to send an alert to your phone any time a crypto in your watch list moves.
 
-The easiest way is to create a GMail account for use with this program, and have it send emails to your mobile phone. Each of the major phone companies have an extension where you can send an email to your phone number and it will come through as an SMS. For example, Sprint's is formatted like the follow: 1234567890@messaging.sprintpcs.com.
+The easiest way is to create a GMail account for use with this program, and have it send emails to your mobile phone. Each of the major phone companies have an extension where you can send an email to your phone number and it will come through as an SMS. For example, Sprint's is formatted like the following: 1234567890@messaging.sprintpcs.com.
 
-If using GMail, you will have to enable insecure apps; there are several articles online for how to do that.
+If using GMail, you will have to enable insecure apps; there are several articles online detailling how best to do that.
 
 Make sure you update the following variables:
 * sender_address - your gmail address
@@ -10,6 +10,7 @@ Make sure you update the following variables:
 * account_password - your gmail address's password
 * ids - string of the cryptos in your watchlist
 * id_list - list of the cryptos in your watchlist
+* main_id - set the main crypto that you want to track
 * pct_chng - you will be alerted when a crypto in your watchlist moves by +/- this percent. Default = 5%
 * dlr_chng - alert you if a crypto (default is ETH) moves by +/- this amount in fiat. Default is 50 USD
 * alert_on_changes - True if you want to get alerts based on pct_chng/dlr_chng; False if you only want alerts at certain times throughout the day
@@ -18,8 +19,18 @@ Make sure you update the following variables:
 
 Note - alert_on_changes, alert_start_hour, and alert_start_minute are in preparation for a future iteration where you will be able to designate specific times to receive alerts
 
+Edit the crontab to run on start. Open the terminal:
+* # crontab -e
+* ***** python3 /path/to/your/script/sms_alert.py
+
+Where 
+||Field|Meaning|Allowed Values||
+| #1 | minute | 0-59 |
+| #2 | hour | 0-23 |
+| #3 | day of month | 1-31 |
+| #4 | month | 1-12 |
+| #5 | day of week | 0-7 |
 
 Future enhancements:
 * All-time high alerts
 * Alerts when a major support / resistance level is reached
-* "Quiet time" where you can set a time range to not send alerts
--- sms_alert.py
+++ sms_alert.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python3
+
 import smtplib, time
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from pycoingecko import CoinGeckoAPI
-from datetime import datetime
+from datetime import datetime, date, timedelta
 import collections
 cg = CoinGeckoAPI()
 
@@ -14,32 +16,21 @@ cg = CoinGeckoAPI()
 ids = 'ethereum,cardano,vechain,harmony,uniswap,solana,neo,algorand'
 # because of the coin_gecko API, we also need to do a little work to get the symbol
 id_list = ['ethereum', 'cardano', 'vechain', 'harmony', 'uniswap', 'solana', 'neo', 'algorand']
-
-# if this is true, you will be alerted on pct_chng and/or dlr_chng
-# set to False if you do not want price alerts
-alert_on_changes = True
-# pct_chng set variable to check percent change
-pct_chng = 5.0
-# dlr_chng set variable to check a dollar change
-dlr_chng = 50.0
+# if you want to track 1 crypto more closely than the others update this var
+main_id = 'ethereum'
 
 #####################################
 #### Configure alert frequency   ####
 #####################################
-# set the time each day you want at least 1 alert to trigger
-# default is 08:30-08:32 according to your system time
-alert_start_hour = 8
-alert_start_minute = 30
-# set how many times per day the alerts will be sent
-# TODO: time check logic
-# alerts_per_day = 4
+# alert frequency is based on what you
+# set your crontab to run
 
 #####################################
 #### Email/SMS alert information ####
 #####################################
 sender_address = "enter_your_gmail_here" 
 receiver_address = "enter_your_phone_number_here@messaging.sprintpcs.com" 
-account_password = "enter_your_gmail_pswd_here" 
+account_password = "enter_your_gmail_pswd_here"
 
 # compose and send the email-sms alert
 # if you're using gmail, you may have to allow insecure apps
@@ -60,11 +51,6 @@ def send_alert(body, sub):
     server.sendmail(sender_address, receiver_address, text)
     server.quit()
     
-# using coin_gecko, get the price of your watch list
-def get_coins():
-    coins = cg.get_price(ids=ids, vs_currencies='usd')
-    return(coins)
-
 # parse the coins to compose your alert
 def parse_coins(coins):
     coins = collections.OrderedDict(sorted(coins.items()))
@@ -75,14 +61,38 @@ def parse_coins(coins):
     return(msg)
 
 # Called when script starts and at a designated time(s)
-def coin_update():
-    coins = get_coins()
+def crypto_alert():
+    coins = cg.get_price(ids=ids, vs_currencies='usd')
     
     msg = parse_coins(coins)
     sub = 'Crypto Alert'
     send_alert(msg, sub)
 
-    return(coins)
+# calculate the pp, resistance, and support
+def calc_technicals(coin_id):
+    coin_tech = cg.get_coin_by_id(id=coin_id, vs_currencies='usd')
+    current = coin_tech['market_data']['current_price']['usd']
+    high = coin_tech['market_data']['high_24h']['usd']
+    low = coin_tech['market_data']['low_24h']['usd']
+   
+    y = date.today() - timedelta(days=1)
+    y = y.strftime('%d-%m-%Y')
+    y = str(y)
+    close = cg.get_coin_history_by_id(coin_id, y)
+    close = close['market_data']['current_price']['usd']
+    
+    pivot_point = (close + high + low)/3
+    r1 = str(round(((2 * pivot_point) - low), 2))
+    s1 = str(round(((2 * pivot_point) - high), 2))
+    s2 = str(round((pivot_point - (high - low)), 2))
+    s3 = str(round((low - 2 * (high - pivot_point)), 2))
+    ath = str(coin_tech['market_data']['ath']['usd'])
+    ath_ch_pct = str(round(coin_tech['market_data']['ath_change_percentage']['usd'], 2))
+    
+    msg = 'Now: ' + str(current) + '\nHigh: ' + str(high) + '\nLow: ' + str(low) +'\nATH: ' + ath + '\nR1: ' + r1 + '\nS1: ' + s1 + '\nS2: ' + s2 + '\nS3: ' + s3
+    sub = symbol_dict[coin_id] + ' Stats'
+    send_alert(msg, sub)
+    return(current)
 
 sym = []
 # get coins from coin_gecko
@@ -99,59 +109,8 @@ for id in id_list:
     symbol_dict[id] = sym[i][0]['symbol'].upper()
     i = i + 1
 
-# run the alert program... always.
-run = True
-
-# trigger times for canned alert
-time1 = datetime.now().time()
-time1 = time1.replace(hour=alert_start_hour, minute=alert_start_minute, second=0, microsecond=0)
-time2 = datetime.now().time()
-time2 = time2.replace(hour=alert_start_hour, minute=alert_start_minute + 1, second=59, microsecond=0)
-# master_time1 = time1
-# master_time2 = time2
-
-# instantiate and send 1 alert when the script first starts
-coins = coin_update()
-print('coins instantiated')
-
-while run:
-    # checks the daily trigger time
-    now = datetime.now()
-    if now.time() > time1 and now.time() < time2:
-        coins = coin_update()
-        print('Timed alert sent at ' + str(now))
+# send alert
+crypto_alert()
+# send technicals for main_id
+calc_technicals(main_id)
     
-    # only send price-check alerts if set to True
-    if alert_on_changes:
-        # gets the price for your watch list for comparison
-        n_coins = get_coins()
-        # I'm tracking ETH, so I set a separate trigger (+/-$50) for ETH
-        eth_chng = n_coins['ethereum']['usd'] - coins['ethereum']['usd']
-        if abs(eth_chng) >= dlr_chng:
-            msg = parse_coins(n_coins)
-            if eth_chng > 0:
-                sub = 'ETH Up'
-            else:
-                sub = 'ETH Down'
-            send_alert(msg, sub)
-            coins = get_coins()
-            print('Compare price updated ' + str(now))
-        else:
-            # all other alerts based on a percent change
-            for k, v in n_coins.items():
-                p1 = coins[k]['usd']
-                p2 = v['usd']
-                pct = ((p2-p1)/p1)*100
-                # change pct_change to what you want to track
-                if abs(pct) >= pct_chng:
-                    t = str(k)
-                    msg = parse_coins(n_coins)
-                    if pct > 0:
-                        sub = symbol_dict[k] + ' Up'
-                    else:
-                        sub = symbol_dict[k] + ' Down'
-                    send_alert(msg, sub)
-                    coins = get_coins()
-                    print(t + ' percent change ' + str(now))
-    # time to pause/sleep before running again
-    time.sleep(100)        
